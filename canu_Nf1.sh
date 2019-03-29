9b97d97341dc598ac82cdc60540a8916cb2edfc0
Eric Morrison <emorrison40@gmail.com>
2019-03-29 14:13:19 -0400

    update workflow

--- canu_Nf1.sh
+++ canu_Nf1.sh
@@ -3,14 +3,14 @@
 module purge
 module load linuxbrew/colsa
 
-#Add minReadLength to call if testing small set of seqs 'minReadLength=500 \'
-#canu is failing on gatekeeper becasue too few reads with small set after filtering 4000 reads
+#stopOnReadQuality=F flag prevents from terminating when filtering reads <1000; this is fine with high coverage
 
 canu \
 -d $HOME/Nf_canu_run0 \
 -p Nf \
 -nanopore-raw $HOME/Nf1_basecalls_albacore/workspace/pass/*.fastq \
 -genomesize=45m \
+stopOnReadQuality=F \
 gnuplotTested=true \
 gridOptions="--partition=shared" \
 gridOptionsJobName=EWM \
